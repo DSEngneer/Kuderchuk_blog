6b3ba706d0bf57368dca1ffceb1bf5b83df447c0
DSEngneer <vladyslav.kuderchuk@oa.edu.ua>
2025-06-22 20:56:43 +0300

    Лабораторна 12. Черги (Queue, Jobs)

--- app/Http/Controllers/Blog/Admin/PostController.php
+++ app/Http/Controllers/Blog/Admin/PostController.php
@@ -8,7 +8,8 @@ use App\Repositories\BlogCategoryRepository;
 use App\Http\Requests\BlogPostUpdateRequest;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
-
+use App\Jobs\BlogPostAfterCreateJob;
+use App\Jobs\BlogPostAfterDeleteJob;
 class PostController extends BaseController
 {
     /**
@@ -57,6 +58,8 @@ class PostController extends BaseController
         $item = (new BlogPost())->create($data); //створюємо об'єкт і додаємо в БД
 
         if ($item) {
+            $job = new BlogPostAfterCreateJob($item);
+            $this->dispatch($job);
             return redirect()
                 ->route('blog.admin.posts.edit', [$item->id])
                 ->with(['success' => 'Успішно збережено']);
@@ -127,6 +130,7 @@ class PostController extends BaseController
         //$result = BlogPost::find($id)->forceDelete(); //повне видалення з БД
 
         if ($result) {
+            BlogPostAfterDeleteJob::dispatch($id)->delay(20);
             return redirect()
                 ->route('blog.admin.posts.index')
                 ->with(['success' => "Запис id[$id] видалено"]);
